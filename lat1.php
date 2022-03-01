c9419a738bd079dd4e8b82b45de0b29c73f07023
hanjarutama <hanjarutama@gmail.com>
2022-03-01 21:26:44 +0700

    First Commit

---
+++ lat1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Halo Nama saya </h1>
+    <?php
+     $nama = "Hildan Hanjar Utama";
+    echo "HALO NAMA SAYA  ".$nama;
+    ?>
+    
+</body>
+</html>
---
+++ lat2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+$a = 10;
+$b = 43;
+//menjumlahkan variabel a dengan variabel b
+echo "Hasil penjumlahan antara ".$a." dan  ".$b." adalah ".$a + $b;
+?>
+    
+</body>
+</html>
